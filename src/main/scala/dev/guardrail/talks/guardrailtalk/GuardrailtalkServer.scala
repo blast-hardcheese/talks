bc350f8096b6b37eb77f66048482cf3d57f8d64f
Devon Stewart <dstewart@tubi.tv>
2022-03-10 12:09:18 -0800

    Tell a joke if we can't find a pet

--- src/main/scala/dev/guardrail/talks/guardrailtalk/GuardrailtalkServer.scala
+++ src/main/scala/dev/guardrail/talks/guardrailtalk/GuardrailtalkServer.scala
@@ -8,21 +8,32 @@ import com.comcast.ip4s._
 
 import fs2.Stream
 
+import org.http4s.client.Client
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits._
 import org.http4s.server.middleware.Logger
 
+import com.icanhazdadjoke.joke.JokeClient
 import dev.guardrail.example.routes
 
 object GuardrailtalkServer {
 
   def stream[F[_]: Async]: Stream[F, Nothing] = {
     for {
-      _ <- Stream.resource(EmberClientBuilder.default[F].build)
+      emberClient <- Stream.resource(EmberClientBuilder.default[F].build)
+
+      jokeClient = JokeClient.httpClient[F](Client.apply({ req =>
+        import org.http4s.MediaRange
+        import org.http4s.headers.Accept
+        import org.http4s.headers.MediaRangeAndQValue
+        // Forcibly inject `Accept: application/*` header to work around https://github.com/guardrail-dev/guardrail/issues/1424
+        val newRequest = req.withHeaders(Accept(MediaRangeAndQValue.withDefaultQValue(MediaRange.`application/*`)))
+        emberClient.run(newRequest)
+      }))
 
       httpApp = (
-        new routes.Resource[F]().routes(new PetRoutes[F])
+        new routes.Resource[F]().routes(new PetRoutes[F](jokeClient))
       ).orNotFound
 
       // With Middlewares in place
--- src/main/scala/dev/guardrail/talks/guardrailtalk/PetRoutes.scala
+++ src/main/scala/dev/guardrail/talks/guardrailtalk/PetRoutes.scala
@@ -3,21 +3,29 @@ package dev.guardrail.talks.guardrailtalk
 import cats.effect.{ IO, Sync }
 import cats.implicits._
 
+import com.icanhazdadjoke.joke.JokeClient
+import dev.guardrail.example.routes.definitions.{ Pet, PetNotFoundResponse }
 import dev.guardrail.example.routes.{ Handler, Resource }
-import dev.guardrail.example.routes.definitions.Pet
+import com.icanhazdadjoke.definitions.Joke
 
-class PetRoutes[F[_]: Sync] extends Handler[F] {
+class PetRoutes[F[_]: Sync](jokeClient: JokeClient[F]) extends Handler[F] {
   val pets = Vector(
     Pet(id=1L, "Fluffy", Some("soft")),
     Pet(id=2L, "Fido", Some("bitey"))
   )
 
   def getPet(respond: Resource.GetPetResponse.type)(id: Long): F[Resource.GetPetResponse] = {
+    val notFound = for {
+      resp <- jokeClient.getJoke()
+    } yield resp.fold(
+      handleOk={ case Joke(joke) =>
+        respond.NotFound(PetNotFoundResponse(s"No pet found with id=${id}", joke))
+      }
+    )
     pets
       .filter(_.id == id)
       .headOption
-      .fold[Resource.GetPetResponse](respond.NotFound(s"No pet found with id=${id}"))(respond.Ok)
-      .pure[F]
+      .fold[F[Resource.GetPetResponse]](notFound.widen)(pet => respond.Ok(pet).pure[F].widen)
   }
 
   def listPets(respond: Resource.ListPetsResponse.type)(tag: Option[Iterable[String]]): F[Resource.ListPetsResponse] = respond.Ok(
