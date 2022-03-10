53a019fd1b872244c1250ad51cc9684468534875
Devon Stewart <dstewart@tubi.tv>
2022-03-09 23:15:44 -0800

    Filter static list of pets by tag

--- src/main/scala/dev/guardrail/talks/guardrailtalk/PetRoutes.scala
+++ src/main/scala/dev/guardrail/talks/guardrailtalk/PetRoutes.scala
@@ -7,10 +7,10 @@ import dev.guardrail.example.routes.{ Handler, Resource }
 import dev.guardrail.example.routes.definitions.Pet
 
 class PetRoutes[F[_]: Sync] extends Handler[F] {
-  def listPets(respond: Resource.ListPetsResponse.type)(): F[Resource.ListPetsResponse] = respond.Ok(
+  def listPets(respond: Resource.ListPetsResponse.type)(tag: Option[Iterable[String]]): F[Resource.ListPetsResponse] = respond.Ok(
     Vector(
       Pet(id=1L, "Fluffy", Some("soft")),
       Pet(id=2L, "Fido", Some("bitey"))
-    )
+    ).filter(pet => tag.forall(_.exists(pet.tag.contains)))
   ).pure[F].widen
 }
