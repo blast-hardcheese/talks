01ee9809962aaa48441111d94ea67019b5e8fbc2
Devon Stewart <dstewart@tubi.tv>
2022-03-09 23:24:28 -0800

    Break out pets list for reuse

--- src/main/scala/dev/guardrail/talks/guardrailtalk/PetRoutes.scala
+++ src/main/scala/dev/guardrail/talks/guardrailtalk/PetRoutes.scala
@@ -7,10 +7,12 @@ import dev.guardrail.example.routes.{ Handler, Resource }
 import dev.guardrail.example.routes.definitions.Pet
 
 class PetRoutes[F[_]: Sync] extends Handler[F] {
+  val pets = Vector(
+    Pet(id=1L, "Fluffy", Some("soft")),
+    Pet(id=2L, "Fido", Some("bitey"))
+  )
+
   def listPets(respond: Resource.ListPetsResponse.type)(tag: Option[Iterable[String]]): F[Resource.ListPetsResponse] = respond.Ok(
-    Vector(
-      Pet(id=1L, "Fluffy", Some("soft")),
-      Pet(id=2L, "Fido", Some("bitey"))
-    ).filter(pet => tag.forall(_.exists(pet.tag.contains)))
+    pets.filter(pet => tag.forall(_.exists(pet.tag.contains)))
   ).pure[F].widen
 }
